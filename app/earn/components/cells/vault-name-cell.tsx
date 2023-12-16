f49a50e7e69d2eed3d02ebfa1cc42260107740dc
Sergey Solovyev <ssolovyev@github.com>
2023-12-16 08:34:22 +0400

    remove new

--- app/earn/components/cells/vault-name-cell.tsx
+++ app/earn/components/cells/vault-name-cell.tsx
@@ -5,25 +5,16 @@ import { toStringNumberFromDecimals } from '../../../shared'
 import { CellWithDescription } from './cell-with-description'
 
 import styles from './vault-name-cell.module.scss'
-import { CellChip } from './cell-chip'
 
 interface Props {
   vault: Vault
 }
 
-const TVL_THRESHOLD = 1e4
-
 export const VaultNameCell: React.FC<Props> = ({ vault }) => {
-  const currentTVL = +toStringNumberFromDecimals(vault.fundAssetsUSD, vault.asset.price.decimals)
   return (
     <CellWithDescription icon={<IconCoin symbol={vault.asset.symbol} width="1.75em" height="1.75em" />}>
       <div className={styles.name}>
         {vault.asset.symbol}
-        {currentTVL < TVL_THRESHOLD && (
-          <CellChip>
-            NEW
-          </CellChip>
-        )}
       </div>
     </CellWithDescription>
   )
