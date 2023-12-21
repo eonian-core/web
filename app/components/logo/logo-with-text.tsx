dcf7a4cc8c93558d9a1c1da0c7eb0a6aec488d4c
Sergey Solovyev <ssolovyev@github.com>
2023-12-21 10:17:17 +0400

    logo size increased up to 12.5%

--- app/components/logo/logo-with-text.tsx
+++ app/components/logo/logo-with-text.tsx
@@ -4,7 +4,7 @@ import EonianLogo from './logo'
 import styles from './logo-with-text.module.scss'
 
 function LogoWithText() {
-  return <InternalLink href="/" className={styles.logo} iconClassName={styles.logoIcon} icon={<EonianLogo />}>
+  return <InternalLink href="/" className={styles.logo} iconClassName={styles.logoIcon} icon={<EonianLogo width="2.25rem" height="2.25rem" />}>
     <span className={styles.name}>Eonian</span>
   </InternalLink>
 }
