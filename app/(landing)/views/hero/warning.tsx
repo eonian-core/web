fffdb2d5d92d0892ce5c79a96c4e71b059d8e569
leovs09 <leovs010@gmail.com>
2024-01-13 22:22:09 +0100

    lint

--- app/(landing)/views/hero/warning.tsx
+++ app/(landing)/views/hero/warning.tsx
@@ -1,26 +1,25 @@
-import React from "react";
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
-
+import React from 'react'
+import { Button, Modal, ModalBody, ModalContent, ModalFooter, ModalHeader, useDisclosure } from '@nextui-org/react'
 
 export interface WarningProps {
-    children: React.ReactNode;
-    title: string;
+  children: React.ReactNode
+  title: string
 }
 
-export default function Warning({title, children}: WarningProps) {
-  const {isOpen, onOpenChange} = useDisclosure({defaultOpen: true});
+export default function Warning({ title, children }: WarningProps) {
+  const { isOpen, onOpenChange } = useDisclosure({ defaultOpen: true })
 
   return (
     <>
       <Modal isOpen={isOpen} onOpenChange={onOpenChange}>
         <ModalContent>
-          {(onClose) => (
+          {onClose => (
             <>
               <ModalHeader className="flex flex-col gap-1">{title}</ModalHeader>
               <ModalBody>
                 {children}
               </ModalBody>
-              
+
               <ModalFooter>
                 <Button color="danger" variant="light" onPress={onClose}>
                   Close
@@ -31,5 +30,5 @@ export default function Warning({title, children}: WarningProps) {
         </ModalContent>
       </Modal>
     </>
-  );
+  )
 }
