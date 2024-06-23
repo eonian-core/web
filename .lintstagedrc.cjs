ae60e18f0802d78fa9a1d0487567168b88fc28ac
leovs09 <leovs010@gmail.com>
2024-06-23 18:40:19 +0200

    test linter

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,7 @@
 const path = require('path')
- 
+
 const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
+  `next lint -c ./.eslintrc.cjs --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
  
