8346eb5a1a24522289e230fecfae4ea4d930734c
leovs09 <leovs010@gmail.com>
2024-06-23 17:31:50 +0200

    remove unused import

--- app/earn/layout.tsx
+++ app/earn/layout.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import type { Metadata } from 'next'
 
 import { overrideMetadata } from '../meta'
-import {clsx} from 'clsx' // only for build test
 import styles from './layout.module.scss'
 
 export default function EarnLayout({ children }: React.PropsWithChildren) {
