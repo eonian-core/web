c405f7db66df98355382aa7ca3e630cb42801157
leovs09 <leovs010@gmail.com>
2024-06-23 19:40:40 +0200

    test linter

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,10 +1,13 @@
 const path = require('path')
 
 const buildEslintCommand = (filenames) =>
-  `npm run format -- --file ${filenames
+  `npm run format`
+ 
+const buildFilesOptions = (filenames) =>
+  `--file ${filenames
     .map((f) => `"${path.relative(process.cwd(), f)}"`)
     .join(' --file ')}`
- 
+
 module.exports = {
   '*.{ts,tsx}': [buildEslintCommand],
 }
