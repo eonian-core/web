55af00be2400038e31929b7876cf4ea64b01c6bc
leovs09 <leovs010@gmail.com>
2024-08-07 23:01:54 +0200

    add different placeholder animation for different vaults

--- app/earn/[...vault]/hooks/use-vault-context.tsx
+++ app/earn/[...vault]/hooks/use-vault-context.tsx
@@ -1,10 +1,12 @@
 import type { PropsWithChildren } from 'react'
 import { createContext, useContext, useEffect, useState } from 'react'
 import { useNumberInputValue } from './use-number-input-value'
+import { coinsPlaceholders, usePlaceholderAnimation } from './use-placeholder-animation'
 import type { Vault } from '@/api'
 import { FormAction } from '@/store/slices/vaultActionSlice'
 import { useWalletWrapperContext } from '@/providers/wallet/wallet-wrapper-provider'
 import { WalletStatus } from '@/providers/wallet/wrappers/types'
+import { getAssetSymbol } from '@/api/vaults/get-asset-symbol'
 
 interface VaultContextType {
   inputValue?: bigint
@@ -46,11 +48,13 @@ export function VaultProvider({ children, vault }: PropsWithChildren<{ vault: Va
 
   const { value, displayValue, onValueChange } = useNumberInputValue(undefined, vault.asset.decimals)
   const { value: placeholderValue = 0n, displayValue: placeholderDisplayValue = '0', onValueChange: onPlaceholderChange } = useNumberInputValue(0n, vault.asset.decimals)
-  usePlaceholderAnimation(onPlaceholderChange, 1000, 300, 50)
   const [formAction, setFormAction] = useState<FormAction>(FormAction.DEPOSIT)
   const [insured, setInsured] = useState(true)
 
   const { status } = useWalletWrapperContext()
+
+  const symbol = getAssetSymbol(vault)
+  usePlaceholderAnimation(coinsPlaceholders[symbol], onPlaceholderChange)
 
   return (
     <VaultContext.Provider
@@ -76,45 +80,4 @@ export function VaultProvider({ children, vault }: PropsWithChildren<{ vault: Va
 
 export function useVaultContext() {
   return useContext(VaultContext)
-}
-
-const placeholderRange = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 36, 37, 38, 39, 40, 41, 42]
-
-const timeout = async (duration: number) => new Promise(resolve => setTimeout(resolve, duration))
-
-function usePlaceholderAnimation(onPlaceholderChange: (value: string | bigint) => void, loadingDelay: number, initialDelay: number, delayDecrease: number) {
-  const [haveRun, setHaveRun] = useState(false)
-
-  useEffect(() => {
-    let exit = false
-
-    const animate = async () => {
-      if (haveRun)
-        return
-
-      await timeout(loadingDelay)
-      let delay = initialDelay
-      await timeout(delay)
-
-      for (const value of placeholderRange) {
-        if (exit)
-          return
-
-        onPlaceholderChange(`${value}`)
-        await timeout(delay)
-
-        if (delay - delayDecrease > 0)
-          delay -= delayDecrease
-      }
-
-      setHaveRun(true)
-    }
-
-    void animate()
-      .catch(console.error)
-
-    return () => {
-      exit = true
-    }
-  }, [onPlaceholderChange, haveRun, setHaveRun])
 }
