a07f82375fe749d9563108fae8ed95a06eb0857d
Solovyev Sergey <mrbroume@gmail.com>
2024-08-10 07:43:44 +0400

    try to fix overlaps

--- app/(landing)/views/partners/partners.tsx
+++ app/(landing)/views/partners/partners.tsx
@@ -38,7 +38,7 @@ interface ItemProps extends Omit<ImageProps, 'alt' | 'src'> {
   src?: string | StaticImageData
   withLabel?: boolean
   contrast?: number
-  width?: number
+  width: number
 }
 
 export default function Partners({ children }: React.PropsWithChildren) {
@@ -76,44 +76,44 @@ function Item({ src, alt, href, withLabel, children, className, contrast, width,
 }
 
 Partners.LogoUnibase = () => (
-  <Item href="https://www.unibase.io/" src={LogoUnibase as StaticImageData} alt="Unibase" contrast={0} />
+  <Item href="https://www.unibase.io/" src={LogoUnibase as StaticImageData} alt="Unibase" contrast={0} width={200} />
 )
-Partners.LogoRido = () => <Item href="https://www.rido.io/" src={LogoRido as StaticImageData} alt="Rido" />
+Partners.LogoRido = () => <Item href="https://www.rido.io/" src={LogoRido as StaticImageData} alt="Rido" width={128} />
 Partners.LogoGenesisWeb3 = () => (
-  <Item href="https://www.genesisweb3.in/" src={GenesisWeb3} alt="GenesisWeb3" withLabel />
+  <Item href="https://www.genesisweb3.in/" src={GenesisWeb3} alt="GenesisWeb3" withLabel width={47} />
 )
-Partners.LogoCheersland = () => <Item href="https://cheersland.org/" src={Cheersland} alt="Cheersland" />
+Partners.LogoCheersland = () => <Item href="https://cheersland.org/" src={Cheersland} alt="Cheersland" width={200} />
 Partners.LogoBitgert = () => <Item href="https://bitgert.com/" src={Bitgert} alt="Bitgert" width={150} />
 Partners.LogoAxen = () => (
-  <Item href="https://axenai.com/">
+  <Item href="https://axenai.com/" width={80}>
     <b style={{ fontSize: '1.55em' }}>AXEN</b>
   </Item>
 )
-Partners.LogoXPNetwork = () => <Item href="https://xp.network/" src={XPBridge} alt="XP Network" />
+Partners.LogoXPNetwork = () => <Item href="https://xp.network/" src={XPBridge} alt="XP Network" width={200} />
 Partners.LogoAutenticRWA = () => (
-  <Item href="https://autentic.capital/" src={AutenticRWA as StaticImageData} alt="Autentic Capital" />
+  <Item href="https://autentic.capital/" src={AutenticRWA as StaticImageData} alt="Autentic Capital" width={91} />
 )
-Partners.LogoUnibit = () => <Item href="https://www.unibit.app/#home" src={Unibit} alt="Unibit" />
-Partners.LogoSaasGo = () => <Item href="https://saasgo.xyz/" src={SaasGo} alt="SaasGo" contrast={0} />
-Partners.LogoColdStack = () => <Item href="https://coldstack.io/" src={ColdStack as StaticImageData} alt="ColdStack" />
+Partners.LogoUnibit = () => <Item href="https://www.unibit.app/#home" src={Unibit} alt="Unibit" width={109} />
+Partners.LogoSaasGo = () => <Item href="https://saasgo.xyz/" src={SaasGo} alt="SaasGo" contrast={0} width={180} />
+Partners.LogoColdStack = () => <Item href="https://coldstack.io/" src={ColdStack as StaticImageData} alt="ColdStack" width={174} />
 Partners.LogoEquationDAO = () => (
-  <Item href="https://equation.org/" src={EquationDAO as StaticImageData} alt="EquationDAO" />
+  <Item href="https://equation.org/" src={EquationDAO as StaticImageData} alt="EquationDAO" width={153} />
 )
-Partners.LogoHYDT = () => <Item href="https://hydtprotocol.com/" src={HYDT as StaticImageData} alt="HYDT" />
-Partners.LogoAveAI = () => <Item href="https://ave.ai/home" src={AveAI} alt="Ave AI" withLabel />
-Partners.LogoCollaby = () => <Item href="https://www.collably.network/" src={Collaby} alt="Collaby Network" />
-Partners.LogoBubbleSwap = () => <Item href="https://bubbleswap.co/" src={BubbleSwap} alt="BubbleSwap" />
+Partners.LogoHYDT = () => <Item href="https://hydtprotocol.com/" src={HYDT as StaticImageData} alt="HYDT" width={180} />
+Partners.LogoAveAI = () => <Item href="https://ave.ai/home" src={AveAI} alt="Ave AI" withLabel width={65} />
+Partners.LogoCollaby = () => <Item href="https://www.collably.network/" src={Collaby} alt="Collaby Network" width={200} />
+Partners.LogoBubbleSwap = () => <Item href="https://bubbleswap.co/" src={BubbleSwap} alt="BubbleSwap" width={200} />
 Partners.LogoHeLa = () => <Item href="https://helalabs.com/" src={HeLa as StaticImageData} alt="HeLa" width={150} />
 Partners.LogoFomoin = () => (
   <Item href="https://fomoin.finance/#/" src={Fomoin as StaticImageData} alt="Fomoin" width={130} />
 )
 Partners.LogoPoison = () => (
-  <Item href="https://poison.finance/" src={Poison} alt="Poison" style={{ width: '100px' }} contrast={0.7} />
+  <Item href="https://poison.finance/" src={Poison} alt="Poison" style={{ width: '100px' }} contrast={0.7} width={100} />
 )
 Partners.LogoBlocknative = () => (
   <Item href="https://www.blocknative.com/" src={Blocknative as StaticImageData} alt="Blocknative" width={150} />
 )
-Partners.LogoOZ = () => <Item href="https://www.openzeppelin.com/" src={OZ} alt="OpenZeppelin" />
+Partners.LogoOZ = () => <Item href="https://www.openzeppelin.com/" src={OZ} alt="OpenZeppelin" width={200} />
 Partners.LogoTheGraph = () => (
   <Item href="https://thegraph.com/" src={TheGraph as StaticImageData} alt="The Graph" width={45} withLabel />
 )
