6fd54a58232a04e3eedc5de3c2825d0fa168dc98
leovs09 <leovs010@gmail.com>
2024-10-27 03:20:46 +0100

    add mobile support to suggestions triggers

--- app/earn/components/header.tsx
+++ app/earn/components/header.tsx
@@ -1,10 +1,11 @@
 'use client'
 
 import { H2, H3 } from '../../components/heading/heading'
+import Mbr from '@/components/mobile-break/mobile-break'
 
 export function Header() {
   return <div>
         <H2>Select Cryptocurrency</H2>
-        <H3>Choose an asset to save and generate passive income</H3>
+        <H3>Choose an asset to save and <Mbr start="mobile"> generate passive income</Mbr></H3>
     </div>
 }
