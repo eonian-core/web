a3f0b7b516576eea6975146e3a6cd5a471311d31
leovs09 <leovs010@gmail.com>
2025-01-15 17:45:11 +0100

    lint fix

--- app/(landing)/views/testimonials/get-tweet.ts
+++ app/(landing)/views/testimonials/get-tweet.ts
@@ -1,5 +1,4 @@
 import type { Tweet } from 'react-tweet/api'
-import { getTweet as _getTweet } from 'react-tweet/api'
 import { tweets } from './tweets'
 
 const fileCache = tweets as unknown as Partial<Record<string, Tweet>>
