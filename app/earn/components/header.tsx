db1a708ccc1988d739501c2d230e3d0dd7b15e12
Vladislav Goncharov <LeoVS010@gmail.com>
2025-03-24 20:39:55 +0100

    Rename save to insure in save page

--- app/earn/components/header.tsx
+++ app/earn/components/header.tsx
@@ -6,6 +6,6 @@ import Mbr from '@/components/mobile-break/mobile-break'
 export function Header() {
   return <div>
         <H2>Select Cryptocurrency</H2>
-        <H3>Choose an asset to save and <Mbr start="mobile"> generate passive income</Mbr></H3>
+        <H3>Choose an asset to insure from hacks <Mbr start="mobile">and generate passive income</Mbr></H3>
     </div>
 }
