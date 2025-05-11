5d33f352e78502ec1ee1335665a3269d8cfb8a0d
Solovyev Sergey <mrbroume@gmail.com>
2025-05-11 14:45:06 +0400

    remove floated promise

--- app/(landing)/views/offer/tokens.tsx
+++ app/(landing)/views/offer/tokens.tsx
@@ -1,34 +1,39 @@
 import type { PropsWithChildren } from 'react'
-import { Suspense } from 'react'
+import { useEffect, useState } from 'react'
 import TokensBody from './tokens-body'
 import styles from './offer.module.scss'
 import { VaultsProvider } from '@/api/protocol/vaults/vaults-context'
 import { CardSkeleton } from '@/components/vault-card/card-skeleton'
 import { getVaultsByChain } from '@/api/protocol/vaults/multicall/fetch-vaults-via-multicall'
 import { ChainId } from '@/providers/wallet/wrappers/helpers'
+import type { Vault } from '@/types'
 
 export default function Tokens({ children }: PropsWithChildren) {
+  const [isLoading, setLoading] = useState(true)
+  const [vaults, setVaults] = useState<Vault[]>([])
+
+  useEffect(() => {
+    const fetchVaults = async () => {
+      const vaults = await getVaultsByChain(ChainId.BSC_MAINNET)
+      setVaults(vaults)
+      setLoading(false)
+    }
+    void fetchVaults()
+  }, [])
+
+  if (isLoading) {
+    return (
+      <div className={styles.tokens}>
+        <CardSkeleton />
+        <CardSkeleton />
+        <CardSkeleton />
+      </div>
+    )
+  }
+
   return (
-    <Suspense
-      fallback={
-        <div className={styles.tokens}>
-          <CardSkeleton />
-          <CardSkeleton />
-          <CardSkeleton />
-        </div>
-      }
-    >
-      <TokensProvider>
-        <TokensBody>{children}</TokensBody>
-      </TokensProvider>
-    </Suspense>
+    <VaultsProvider currentChainVaults={vaults}>
+      <TokensBody>{children}</TokensBody>
+    </VaultsProvider>
   )
-}
-
-const promise = getVaultsByChain(ChainId.BSC_MAINNET)
-
-async function TokensProvider({ children }: PropsWithChildren) {
-  const vaults = await promise
-
-  return <VaultsProvider currentChainVaults={vaults}>{children}</VaultsProvider>
 }
